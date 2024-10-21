656ade5b4deb5c419f2fbc78be792b4c909bfd65
guidng <guiguidonega@gmail.com>
2024-10-21 16:50:49 -0300

    EX2 corrigido novamente

--- funcoes.py
+++ funcoes.py
@@ -10,7 +10,7 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
 
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     if nome_navio not in frota.keys():
-        frota[nome_navio] =  define_posicoes(linha,coluna,orientacao,tamanho)
+        frota[nome_navio] =  [define_posicoes(linha,coluna,orientacao,tamanho)]
     else:
         frota[nome_navio].append(define_posicoes(linha,coluna,orientacao,tamanho))
     return frota

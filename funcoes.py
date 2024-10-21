bd57e5ca58ee73e167121a76c68b7c17a09cb67a
guidng <guiguidonega@gmail.com>
2024-10-21 16:45:25 -0300

    EX2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,25 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
         else:
             coluna+=1
     return lista_posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    if nome_navio not in frota.keys():
+        frota[nome_navio] =  define_posicoes(linha,coluna,orientacao,tamanho)
+    else:
+        frota[nome_navio].append(define_posicoes(linha,coluna,orientacao,tamanho))
+    return frota
+    
+
+frota = {
+  "navio-tanque":[
+    [[6,1],[6,2],[6,3]]
+  ]
+}
+nome_navio = 'navio-tanque'
+linha = 4
+coluna = 7
+orientacao = 'vertical'
+tamanho = 3
+
+resultado = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+print(resultado)

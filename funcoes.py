900f6d07fd335caaf8c7f77070f8d9dc75a61289
Caio_Rosa <teste@example.com>
2024-10-25 22:06:33 -0300

    EX4, EX4, EX5

--- funcoes.py
+++ funcoes.py
@@ -26,3 +26,41 @@ def faz_jogada(tabuleiro, linha, coluna):
 
     return tabuleiro
 
+# EX4
+def posiciona_frota(frota):
+    if len(frota.keys())>0:
+        tab_defesa=[]
+        for y in range(10):
+            tab_defesa.append([0] *10)
+        for barco,pos in frota.items():
+            for posic in pos:
+                for posi in posic:
+                    tab_defesa[posi[0]][posi[1]]=1
+        return tab_defesa
+
+# EX5
+def afundados(frota,tabuleiro):
+    n=0
+    for barco,pos in frota.items():
+        for posic in pos:
+            v=0
+            for posi in posic:
+                if tabuleiro[posi[0]][posi[1]]!='X':
+                    v=1
+            if v==0:
+                n+=1
+    return n
+
+# EX6
+def posicao_valida(frota,linha,coluna,orientacao,tamanho):
+    resp=True
+    posicoes=define_posicoes(linha,coluna,orientacao,tamanho)
+    for posi in posicoes:
+        if posi[0]>=10 or posi[0]<0 or posi[1]>=10 or posi[1]<0:
+            resp=False
+        for pos in frota.values():
+            for posic in pos:
+                for posicao in posic:
+                    if posicao[0]==posi[0] and posicao[1]==posi[1]:
+                        resp=False
+    return resp
---
+++ programa.py
@@ -0,0 +1,6 @@
+# EX7
+linha=int(input('Qual linha:'))
+coluna=int(input('Qual coluna:'))
+orientacao=int(input('Qual orientação:'))
+if orientacao==1:orientacao='vertical'
+else:orientacao='horizontal'
